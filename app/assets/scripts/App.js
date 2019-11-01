1d4c4947173b942d4d849e18582762f5620c2384
Claudia <wsolution.heim@gmail.com>
2019-11-01 20:40:31 +0100

    Scroll Effekt ein-ausblenden mit Zoom-Effekt

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,11 @@
 // Import
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 // class for BluePrint - creates a new Object which uses this class
 var mobileMenu = new MobileMenu();
+
+
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
