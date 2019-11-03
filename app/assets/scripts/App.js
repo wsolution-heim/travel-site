be8e3f02cc5071e635b897cc07084e48c6837b0b
Claudia <wsolution.heim@gmail.com>
2019-11-03 10:26:01 +0100

    Sticky Header - Active Links - Smooth Scrolling Page

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,6 +2,7 @@
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
+import StickyHeader from './modules/StickyHeader';
 
 // class for BluePrint - creates a new Object which uses this class
 var mobileMenu = new MobileMenu();
@@ -9,3 +10,4 @@ var mobileMenu = new MobileMenu();
 
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
+var stickyHeader = new StickyHeader();
