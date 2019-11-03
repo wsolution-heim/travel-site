597b87ad055883adba813451671e8bb759d65c9b
Claudia <wsolution.heim@gmail.com>
2019-11-03 12:13:50 +0100

    Contact overlay finished

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,10 @@
 // Import
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import Modal from './modules/Modal'
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
+
 
 // class for BluePrint - creates a new Object which uses this class
 var mobileMenu = new MobileMenu();
@@ -11,3 +13,4 @@ var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal;
